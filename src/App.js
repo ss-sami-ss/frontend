a988d105af5e5cdf34a1b8a31995ba3ec62944f0
Saman Gh <so_sami_so@yahoo.com>
2020-12-29 22:51:39 +0330

    transition-to-react-

--- src/App.js
+++ src/App.js
@@ -1,134 +1,70 @@
-import logo from './logo.svg';
+import React from 'react';
+import data from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    <aside class="sidebar">
-        <h3>Shopping Categories</h3>
-        <button class="sidebar-close-button" onclick="closeMenu()"> x </button>
-        <ul>
-            <li>
-                <a href="index.html">Pants</a>
-            </li>
-            <li>
-                <a href="index.html">Shirts</a>
-            </li>
-        </ul>
-    </aside>
-    <main class="main">  
-        <div class="content">
-            <ul class="products">    
-                <li>
-                    <div class="product">
-                        <img class="product-image" src="images/d1.jpg" alt="product">
-                        <div class="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div class="product-brand">Nike</div>
-                        <div class="product-price">60$</div>
-                        <div class="product-rating">4.5 Stars (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div class="product">
-                        <img class="product-image" src="images/d1.jpg" alt="product">
-                        <div class="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div class="product-brand">Nike</div>
-                        <div class="product-price">60$</div>
-                        <div class="product-rating">4.5 Stars (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div class="product">
-                        <img class="product-image" src="images/d1.jpg" alt="product">
-                        <div class="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div class="product-brand">Nike</div>
-                        <div class="product-price">60$</div>
-                        <div class="product-rating">4.5 Stars (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div class="product">
-                        <img class="product-image" src="images/d1.jpg" alt="product">
-                        <div class="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div class="product-brand">Nike</div>
-                        <div class="product-price">60$</div>
-                        <div class="product-rating">4.5 Stars (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div class="product">
-                        <img class="product-image" src="images/d1.jpg" alt="product">
-                        <div class="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div class="product-brand">Nike</div>
-                        <div class="product-price">60$</div>
-                        <div class="product-rating">4.5 Stars (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div class="product">
-                        <img class="product-image" src="images/d1.jpg" alt="product">
-                        <div class="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div class="product-brand">Nike</div>
-                        <div class="product-price">60$</div>
-                        <div class="product-rating">4.5 Stars (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div class="product">
-                        <img class="product-image" src="images/d1.jpg" alt="product">
-                        <div class="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div class="product-brand">Nike</div>
-                        <div class="product-price">60$</div>
-                        <div class="product-rating">4.5 Stars (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div class="product">
-                        <img class="product-image" src="images/d1.jpg" alt="product">
-                        <div class="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div class="product-brand">Nike</div>
-                        <div class="product-price">60$</div>
-                        <div class="product-rating">4.5 Stars (10 Reviews)</div>
-                    </div>
-                </li>
 
-            </ul>
+    const openMenu= () =>{
+        document.querySelector(".sidebar").classList.add("open");
+    }
+    const closeMenu= () =>{
+        document.querySelector(".sidebar").classList.remove("open");
+    }
 
+    return (
+
+      <div className="grid-container">
+            <header className="header">
+                <div className="brand">
+                    <button onClick={openMenu}>
+                        ☰
+                    </button>
+                    <a href="index.html">Aamazona</a>
+                </div>
+                <div className="header-links">   
+                    <a href="cart.html">Cart</a>
+                    <a href="Singin.html">Sing In</a>
+                </div>
+            </header>
+            <aside className="sidebar">
+                <h3>Shopping Categories</h3>
+                <button className="sidebar-close-button" onClick={closeMenu}> x </button>
+                <ul>
+                    <li>
+                        <a href="index.html">Pants</a>
+                    </li>
+                    <li>
+                        <a href="index.html">Shirts</a>
+                    </li>
+                </ul>
+            </aside>
+            <main className="main">  
+                <div className="content">
+                    <ul className="products"> 
+                     {
+                        data.products.map(product => 
+                        <li>
+                            <div className="product">
+                                <img className="product-image" src={product.image} alt="product"/>
+                                <div className="product-name">
+                                    <a href="product.html">{product.name}</a>
+                                </div>
+                                <div className="product-brand">{product.brand}</div>
+                                <div className="product-price">${product.price}</div>
+                                <div className="product-rating">{product.rating} stars ({product.numReviews})</div>
+                            </div>
+                        </li>)
+                        
+                     }   
+                        
+
+                    </ul>
+
+                </div>
+            </main>
+            <footer className="footer">All right reserved.</footer>
         </div>
-    </main>
-    <footer class="footer">All right reserved.</footer>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,51 @@
+//eslint-disable-next-line
+export default{
+    products:
+    [
+        {
+            name:'Slim Shirt',
+            category:'Shirts',
+            image:'/images/d1.jpg',
+            price:60,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10
+        },
+        {
+            name:'Fit Shirt',
+            category:'Shirts',
+            image:'/images/d1.jpg',
+            price:50,
+            brand:'Nike',
+            rating:4.2,
+            numReviews:5
+        },
+        {
+            name:'Best Pants',
+            category:'Pants',
+            image:'/images/d1.jpg',
+            price:70,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:8
+        },
+        {
+            name:'Fit Shirt',
+            category:'Shirts',
+            image:'/images/d1.jpg',
+            price:50,
+            brand:'Nike',
+            rating:4.2,
+            numReviews:5
+        },
+        {
+            name:'Best Pants',
+            category:'Pants',
+            image:'/images/d1.jpg',
+            price:70,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:8
+        },
+    ]
+} 
