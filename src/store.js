09367893286468bc9aa293106165f342b384a85f
Saman Gh <so_sami_so@yahoo.com>
2021-01-10 14:54:19 +0330

    implement add to cart

--- src/store.js
+++ src/store.js
@@ -1,13 +1,21 @@
-import {createStore,combineReducers, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducers";
 
-const initialState={};
-const reducer=combineReducers({
-    productList:productListReducer,
-})
+const initialState = {};
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
 
-const composeEnhandcer=window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
-const store=createStore(reducer,initialState,compose(applyMiddleware(thunk)));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export default store;
